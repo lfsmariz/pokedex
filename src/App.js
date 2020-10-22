4084f999244f3caeec4273763a19d3a2d4794643
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-22 19:17:41 -0300

    linhas de código antigo removidas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-// import './components/list-pokemon/list-pokemon'
-// import Pokelistelement from './components/list-pokemon/list-pokemon';
+
 import Container from './components/Container/Container';
 
 function App() {
   return (
     <div className='App'>
-      {/* <Pokelistelement value='150'/> */}
       <Container />
     </div>
   );
