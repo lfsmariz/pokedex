2ac39e5184a43f3150b2845f46730089162b38c6
Luiz F S Mariz <lfsmariz@gmail.com>
2020-10-22 12:25:12 -0300

    extract new values(height)

--- src/lib/pokelist.js
+++ src/lib/pokelist.js
@@ -1,34 +1,45 @@
-const fetch = require('node-fetch')
-const POKEAPI = 'https://pokeapi.co/api/v2/pokemon?limit='
+const fetch = require('node-fetch');
+const POKEAPI = 'https://pokeapi.co/api/v2/pokemon?limit=';
 
 const pokeList = async (range) => {
   const listPoke = await fetch(`${POKEAPI}${range}`)
     .then(result=>result.json())
-    .then(result => result.results)
+    .then(result => result.results);
 
-  return listPoke
+  return listPoke;
 }
 
 const capturePokemon = async (url) => {
   const pokemon = await fetch(url)
-    .then(result => result.json())
+    .then(result => result.json());
 
-  const {name, id, sprites: {front_default, front_shiny}, weight, types : typepokemon} = pokemon
+  const {
+    height,
+    name, 
+    id, 
+    sprites: {other: {'official-artwork': { front_default }}}, 
+    weight, 
+    types : typepokemon,
+    species : { url : urlDescription }
+  } = pokemon;
   
-  const redefinePokemon ={name, id, front_default, front_shiny, weight, typepokemon}
+  const redefinePokemon ={name, id, front_default, weight, height, typepokemon};
 
-  return redefinePokemon
+  return redefinePokemon;
 }
 
 const createListPokemon = async (range) => {
-  const list = await pokeList(range)
+  const list = await pokeList(range);
 
   const pokemons = await Promise.all(list.map(async ({url}) => {
-    const pokeData = await capturePokemon(url)
-    return pokeData
+    const pokeData = await capturePokemon(url);
+    return pokeData;
   }))
 
-  return pokemons
+  return pokemons;
 }
 
-export default createListPokemon
+createListPokemon()
+  .then(result => console.log(result));
+
+// export default createListPokemon
