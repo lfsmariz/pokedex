c2043541f86be5a53101397f2433d1f96d95d8cc
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-23 09:31:33 -0300

    exibe dados dos pokémons na lista

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import createListPokemon from '../../lib/pokelist';
+
 import './PokeList.css';
 
 class PokeList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemonList: []
+    };
+  };
+
+  componentDidMount() {
+    createListPokemon(250).then(data => this.setState({ pokemonList: data }));
+  };
+
   render() {
     return (
       <div className="container-pokelist">
-        PokeList
+        <ol>
+          {
+            this.state.pokemonList.map(pokemon => (
+              <li key={pokemon.id}>Nº{String(pokemon.id).padStart(3, '0')} - {pokemon.name}</li>
+            ))
+          }
+        </ol>
       </div>
     );
   };
