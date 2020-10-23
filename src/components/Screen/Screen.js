13b490c9419ab8ccf1183ed3bb543809b2270cb5
Daniel Carvalho <daanwhat@gmail.com>
2020-10-23 16:11:58 -0300

    CSS da Tela de Gameboy otimizado

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './Screen.css';
 
-import canvas from './images/canvas.svg';
-
 class Screen extends Component {
   render() {
     const { pokemonList: { front_default } } = this.props;
 
     return (
-      <div className="container-screen">
-
-        <img className="screen" src={canvas} alt="screen" />
+      <div className="gameboy-screen">
         <img className="sample" src={front_default} alt="pokémon" />
-
       </div>
     );
   };
