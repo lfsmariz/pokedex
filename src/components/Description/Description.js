15c4a9712a180fafb19ff54b988997785696cde9
Luiz F S Mariz <lfsmariz@gmail.com>
2020-10-23 11:17:58 -0300

    create elements default description

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
-import './Description.css';
+import React, { Component } from "react";
+import "./Description.css";
 
 class Description extends Component {
-
   render() {
+    const {
+      id = 0,
+      name = "unknown",
+      weight = 0,
+      height = 0,
+      typepokemon = [],
+      front_default = "",
+      descriptionPoke = "",
+    } = this.props.pokemon;
     return (
       <div className="container-description">
-        Description
+        <h2 className="name-id">
+          {String(id).padStart(3, 0)} - {name}
+        </h2>
+        <p className="description">{descriptionPoke}</p>
+        <div className="characteristics">
+          <div>
+            <p className="characteristics-weight">{weight}</p>
+            <p className="characteristics-height">{height}</p>
+            {typepokemon.map(type=> (
+              <p key={type}>element</p>
+            ))}
+          </div>
+          <div>
+            {/* <img
+              src="https://www.pngfind.com/pngs/m/301-3015958_246kib-900x900-grass-medallion-by-zekrom-9-d7x8iod.png"
+              alt="types"
+            /> */}
+          </div>
+        </div>
       </div>
     );
   };
